ff0806261b83736eb6eda460732aa1906cd9f735
Yukio <DivinusMessor@users.noreply.github.com>
2022-03-29 00:42:33 -0700

    added

--- week4/Lab4/fifo.c
+++ week4/Lab4/fifo.c
@@ -1,4 +1,12 @@
-// CST 334 Lab 4 – FIFO Page Replacement Simulation File I/O Template
+// CST 334 Lab 4 – FIFO Page Replacement Simulation File I/O 
+// Template
+
+/*
+# Name: Yukio Rivera
+# Date: 3/22/2022
+# Title: Lab 4 
+# Description: Gain experience with page replacement
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,9 +27,12 @@ int main(int argc, char *argv[]){
     ref_page cache[C_SIZE]; // Cache that stores pages 
     char pageCache[100]; // Cache that holds the input from test file
 
-    q = queue_create(); // q that I created 
+    q = queue_create(); // queue that I created 
     int i;
     int totalFaults = 0; // keeps track of the total page faults
+    int totalAccesses = 0;
+    int totalHits = 0;
+    double hitRate = 0;
     
     for (i = 0; i < C_SIZE; i++){//initialise cache array  
         cache[i].pageno = -1;
@@ -29,6 +40,7 @@ int main(int argc, char *argv[]){
 
     while (fgets(pageCache, 100, stdin)){
     	int page_num = atoi(pageCache); //Stores number read from file as int
+        totalAccesses++;
 		
 		if (!queue_find(q, page_num)){
         
@@ -37,10 +49,14 @@ int main(int argc, char *argv[]){
             }
             totalFaults++;
             enqueue(q, page_num);
+        } else {
+            totalHits++;
         }
     }
-
-    printf("%d Total Page Faults", totalFaults);
+    hitRate = ((double)totalHits/(double)totalAccesses) * 100;
+    printf("%d Total Accesses, %d Total Hits, %d Total Page Faults,  %0.2f%% Hit Rate.\n",
+        totalAccesses, totalHits, totalFaults, hitRate);
+    queue_destroy(q);
     return 0;
 
 }
