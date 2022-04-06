a821ec93e4ccbbac2ea0e905af2caade130d8728
Yukio <DivinusMessor@users.noreply.github.com>
2022-04-05 18:50:11 -0700

    step 2

--- week5/test.c
+++ week5/test.c
@@ -1,52 +1,88 @@
-#include <stdlib.h>
-#include <pthread.h>
-#include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 
-// #define N 10
-// #define M 10
-// #define L 10
-int size = 8;
-int i, j, k;
-
-
-int main()
+//Each thread computes single element in the resultant matrix
+void *mult(void* arg)
 {
-    
+    int *data = (int *)arg;
+    int k = 0, i = 0;
+     
+    int x = data[0];
+    for (i = 1; i <= x; i++)
+           k += data[i]*data[i+x];
+     
+    int *p = (int*)malloc(sizeof(double));
+        *p = k;
+     
+//Used to terminate a thread and the return value is passed as a pointer
+    pthread_exit(p);
+}
+
+int main() {
+   	const int gridSize = 3;
+   	// const int gridSize = 1024;
     srand(time(NULL));
+    int i;
+    int j;
+    int k;
 
-    double matrixA[size][size];
-    double matrixB[size][size];
-    // double matrixC[N][L];
-    //int i,j,k;
+    // Declare the three matrices
+    double** matrixA = (double**)malloc(sizeof(double*) *gridSize);
+    double** matrixB = (double**)malloc(sizeof(double*) *gridSize);
+    double** matrixC = (double**)malloc(sizeof(double*) *gridSize);
+    for (i = 0; i < gridSize; i++) {
+        // matrixA[i] = (double*)malloc(sizeof(double)*gridSize);
+        // matrixB[i] = (double*)malloc(sizeof(double)*gridSize);
+        // matrixC[i] = (double*)malloc(sizeof(double)*gridSize);
+        matrixA[i] = (double*)calloc(gridSize*gridSize, sizeof(double));
+        matrixB[i] = (double*)calloc(gridSize*gridSize, sizeof(double));
+        matrixC[i] = (double*)calloc(gridSize*gridSize, sizeof(double));
+    }
 
-    
-    for (int i = 0; i < size; i++){
-        for (int j = 0; j < size; j++){
+    // Generate random numbers for our two matrices
+    for (i = 0; i < gridSize; i++) {
+        for (j = 0; j < gridSize; j++) {
             matrixA[i][j] = rand() % 10;
             matrixB[i][j] = rand() % 10;
         }
     }
 
-    
-    // Displaying matA        
-    for (int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++){
-            printf("%g ", matrixA[i][j]);
+    // Multi Threaded Multiplication
+    // pthread_t p;
+    // double toCalc[gridSize*2];
+    // printf("VAL TEST: ", toCalc[(gridSize*2)-1]);
+    // pthread_create(&p, NULL, calculate, );
+
+    // DISPLAY section
+    for (i = 0; i < gridSize; i++) {
+        for (j = 0; j < gridSize; ++j) {
+            if (j == (gridSize - 1)) {
+                printf(" %g |", matrixA[i][j]);
+                printf("\n");
+            } else {
+                printf(" %g ", matrixA[i][j]);
+            }
         }
-        printf("\n");
     }
 
-    printf("-------------------\n");
-             
-    // Displaying matB               
-    for (int i = 0; i < size; i++){
-        for(int j = 0; j < size; j++)
-            printf("%g ",matrixB[i][j]);
-        printf("\n");   
+    printf("============ \n");
+
+    for (i = 0; i < gridSize; i++) {
+        for (j = 0; j < gridSize; ++j) {
+            if (j == (gridSize - 1)) {
+                printf(" %g |", matrixB[i][j]);
+                printf("\n");
+            } else {
+                printf(" %g ", matrixB[i][j]);
+            }
+        }
     }
 
-    int max = size*size;
+    printf("============ \n");
+
+    int max = gridSize*gridSize;
      
      
     //declaring array of threads of size r1*c2       
@@ -55,39 +91,45 @@ int main()
      
     int count = 0;
     int* data = NULL;
-    for (i = 0; i < size; i++)
-        for (j = 0; j < size; j++)
-               {
+    for (i = 0; i < gridSize; i++)
+        for (j = 0; j < gridSize; j++) {
                 
                //storing row and column elements in data
             data = (int *)malloc((20)*sizeof(int));
-            data[0] = c1;
+            data[0] = gridSize;
      
-            for (k = 0; k < c1; k++)
-                data[k+1] = matA[i][k];
+            for (k = 0; k < gridSize; k++)
+                data[k+1] = matrixA[i][k];
      
-            for (k = 0; k < r2; k++)
-                data[k+c1+1] = matB[k][j];
+            for (k = 0; k < gridSize; k++)
+                data[k+gridSize+1] = matrixB[k][j];
               
              //creating threads
                 pthread_create(&threads[count++], NULL,
                                mult, (void*)(data));
                  
-                    }
+            }
      
     printf("RESULTANT MATRIX IS :- \n");
     for (i = 0; i < max; i++)
     {
-      void *k;
+        void *k;
        
-      //Joining all threads and collecting return value
-      pthread_join(threads[i], &k);
+        //Joining all threads and collecting return value
+        pthread_join(threads[i], &k);
             
            
-          int *p = (int *)k;
-      printf("%d ",*p);
-      if ((i + 1) % c2 == 0)
-          printf("\n");
+        int *p = (int *)k;
+        
+        printf("%d ",*p);
+        if ((i + 1) % gridSize == 0)
+            printf("\n");
+        
     }
-    
+
+    return 0;
+}
+
+void* calculate(void* arg) {
+
 }
--- week5/test2.c
+++ week5/test2.c
@@ -1,80 +1,99 @@
-#include <time.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
 
-int main() {
-    const int gridSize = 3;
-    // const int gridSize = 1024;
-    srand(time(NULL));
-    int i;
-    int j;
+#define N 1024	   //# rows of matrix A
+#define M 1024	   //# columns  matrix A & # rows matrix B
+#define L 1024	   //# colummns matrix B
+#define NTHREADS N //# threads
+pthread_t threads[NTHREADS];
 
-    // Declare the three matrices
-    double** matrixA = (double**)malloc(sizeof(double*) *gridSize);
-    double** matrixB = (double**)malloc(sizeof(double*) *gridSize);
-    double** matrixC = (double**)malloc(sizeof(double*) *gridSize);
-    for (i = 0; i < gridSize; i++) {
-        matrixA[i] = (double*)malloc(sizeof(double)*gridSize);
-        matrixB[i] = (double*)malloc(sizeof(double)*gridSize);
-        matrixC[i] = (double*)malloc(sizeof(double)*gridSize);
-    }
+double matrixA[N][M]; // initialize matrix A
+double matrixB[M][L]; // initialize matrix B
+double matrixC[N][L]; // initialize result matrix C
 
-    // Generate random numbers for our two matrices
-    for (i = 0; i < gridSize; i++) {
-        for (j = 0; j < gridSize; j++) {
-            matrixA[i][j] = rand();
-            matrixB[i][j] = rand();
-        }
-    }
-
-    //  Multi Threaded Multiplication
-    pthread_t p;
-    // double toCalc[gridSize*2];
-    // printf("VAL TEST: ", toCalc[(gridSize*2)-1]);
-    // pthread_create(&p, NULL, calculate, );
-
-    // DISPLAY section
-    for (i = 0; i < gridSize; i++) {
-        for (j = 0; j < gridSize; ++j) {
-            if (j == (gridSize - 1)) {
-                printf(" %g |", matrixA[i][j]);
-                printf("\n");
-            } else {
-                printf(" %g ", matrixA[i][j]);
-            }
-        }
-    }
-
-    printf("============ \n");
-
-    for (i = 0; i < gridSize; i++) {
-        for (j = 0; j < gridSize; ++j) {
-            if (j == (gridSize - 1)) {
-                printf(" %g |", matrixB[i][j]);
-                printf("\n");
-            } else {
-                printf(" %g ", matrixB[i][j]);
-            }
-        }
-    }
-
-    printf("============ \n");
-
-    // for (i = 0; i < gridSize; i++) {
-    //     for (j = 0; j < gridSize; ++j) {
-    //         if (j == (gridSize - 1)) {
-    //             printf(" %g |", matrixC[i][j]);
-    //             printf("\n");
-    //         } else {
-    //             printf(" %g ", matrixC[i][j]);
-    //         }
-    //     }
-    // }
-
-    return 0;
+void *math(void *arg)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < L; j++)
+		{
+			double temp = 0;
+			for (int k = 0; k < M; k++)
+			{
+				temp += matrixA[i][k] * matrixB[k][j];
+			}
+			matrixC[i][j] = temp;
+		}
+	}
 }
 
-void* calculate(void* arg) {
+int main()
+{
+	srand(time(NULL));
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+		{
+			matrixA[i][j] = rand() % 10;
+			matrixB[i][j] = rand() % 10;
+		}
 
+	// printf("========Matrix A==========\n");
+	// for (int i = 0; i < N; i++)
+	// {
+	// 	for (int j = 0; j < M; ++j)
+	// 	{
+	// 		if (j == (M - 1))
+	// 		{
+	// 			printf(" %g  ", matrixA[i][j]);
+	// 			printf("\n");
+	// 		}
+	// 		else
+	// 		{
+	// 			printf(" %g ", matrixA[i][j]);
+	// 		}
+	// 	}
+	// }
+	// printf("=========Matrix B==========\n");
+	// for (int i = 0; i < M; i++)
+	// {
+	// 	for (int j = 0; j < L; ++j)
+	// 	{
+	// 		if (j == (L - 1))
+	// 		{
+	// 			printf(" %g  ", matrixB[i][j]);
+	// 			printf("\n");
+	// 		}
+	// 		else
+	// 		{
+	// 			printf(" %g ", matrixB[i][j]);
+	// 		}
+	// 	}
+	// }
+	// Make threads
+	for (int i = 0; i < NTHREADS; i++)
+	{
+		pthread_create(&threads[i], NULL, math, (void *)i);
+	}
+	// join threads
+	for (int i = 0; i < NTHREADS; i++)
+	{
+		pthread_join(threads[i], NULL);
+	}
+	printf("========Matrix C=========\n");
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < L; j++)
+		{
+			if (j == (L - 1))
+			{
+				printf(" %g ", matrixC[i][j]);
+				printf("\n");
+			}
+			else
+			{
+				printf(" %g ", matrixC[i][j]);
+			}
+		}
+	}
 }
